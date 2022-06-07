2e32af71a810011df9da5bf2e48b1d004cfcd137
DESKTOP-K0HS5QI\Dell <vaideska@mail.ru>
2022-06-07 20:02:15 +0300

    core functionality

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,7 @@ const initialListUser = [
 ];
 
 const userTableElement = document.querySelector('.user-table');
-
+const formUserData = document.querySelector('.user-data-form');
 const rowTemplate = document.querySelector('#template-table-row')
   .content.querySelector('.template-row');
 
@@ -26,29 +26,54 @@ const handleDeleteRowClick = (event) => {
   event.target.closest('.template-row').remove();
 }
 
+const closeEditMode = () => {
+  const editRow = document.querySelector('.edit-mode');
+  if (editRow) editRow.classList.remove('edit-mode');
+}
+
 const handleEditRowClick = ({name, phone}) => (event) => {
   const rowElement = event.target.closest('.template-row');
-  console.log(name, phone);
+  closeEditMode();
+  rowElement.classList.add('edit-mode');
 }
 
 const getRowTable = (userData) => {
   const rowElement = rowTemplate.cloneNode(true);
-  const cellNameElement = rowElement.querySelector('.cell-name');
-  const cellPhoneElement = rowElement.querySelector('.cell-phone');
+  const cellNameElement = rowElement.querySelector('.cell-name-value');
+  const cellPhoneElement = rowElement.querySelector('.cell-phone-value');
+  const cellNameInput = rowElement.querySelector('.cell-name-input');
+  const cellPhoneInput = rowElement.querySelector('.cell-phone-input');
   const editButton = rowElement.querySelector('.cell-edit-btn');
   const deleteButton = rowElement.querySelector('.cell-delete-btn');
+  const saveButton = rowElement.querySelector('.cell-save-btn');
 
   cellNameElement.textContent = userData.name;
   cellPhoneElement.textContent = userData.phone;
+  cellNameInput.value = userData.name;
+  cellPhoneInput.value = userData.phone;
 
   editButton.addEventListener('click', handleEditRowClick(userData));
   deleteButton.addEventListener('click', handleDeleteRowClick);
+  saveButton.addEventListener('click', () => {
+    cellNameElement.textContent = cellNameInput.value;
+    cellPhoneElement.textContent = cellPhoneInput.value;
+    closeEditMode();
+  });
 
   return rowElement;
 }
 
-const renderRows = (userData, wrap) => {
+const renderRow = (userData, wrap) => {
   wrap.append(getRowTable(userData));
 }
 
-initialListUser.forEach((userData) => renderRows(userData, userTableElement));
+const handleAddUserDataSubmit = (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  renderRow(Object.fromEntries(formData), userTableElement);
+  event.target.reset();
+}
+
+formUserData.addEventListener('submit', handleAddUserDataSubmit);
+
+initialListUser.forEach((userData) => renderRow(userData, userTableElement));
